5ddf7276613aca1670a89e92b01621ad91b0d652
Missing-C <837103414@qq.com>
2022-11-23 16:48:58 +0800

    代码格式修改，添加大图取色

--- src/components/dialog/colorPicker.vue
+++ src/components/dialog/colorPicker.vue
@@ -1,12 +1,12 @@
 <!--
  * @Author: 蓝山
  * @Date: 2022-11-22 17:15:02
- * @FilePath: \trunk\src\components\dialog\colorPicker.vue
+ * @FilePath: \Miss\src\components\dialog\colorPicker.vue
  * @LastEditors: 蓝山
- * @LastEditTime: 2022-11-22 18:08:07
+ * @LastEditTime: 2022-11-23 15:22:36
 -->
 <script>
-import { showLoading, hideLoading } from "../../lib/loading.js";
+import { showLoading, hideLoading } from '../../lib/loading.js';
 export default {
   mixins: [showLoading, hideLoading],
   data() {
@@ -16,8 +16,10 @@ export default {
       dialogVisible: false,
       // 大图参数
       uploadFileBig: null,
-      textBig: "",
-      colorBig: "",
+      textBigRgba: '',
+      colorBigRgba: '',
+      textBigRgb: '',
+      colorBigRgb: '',
       pixelBig: null,
       ctxBig: null,
     };
@@ -34,52 +36,56 @@ export default {
       const that = this;
       var img = new Image();
       img.src = this.uploadFileBig;
-      var localImg = document.getElementById("dialogImg");
-      var imgCanvas = document.getElementById("dialogImgCanvas");
+      var localImg = document.getElementById('dialogImg');
+      var imgCanvas = document.getElementById('dialogImgCanvas');
       console.log(localImg);
       imgCanvas.width = localImg.width;
       imgCanvas.height = localImg.height;
-      this.ctxBig = imgCanvas.getContext("2d");
+      this.ctxBig = imgCanvas.getContext('2d');
       img.onload = () => {
         that.ctxBig.drawImage(img, 0, 0, localImg.width, localImg.height);
         that.pixelBig = that.ctxBig.getImageData(1, 1, 1, 1);
         let rgba = that.getRgba(188, 8);
-        this.colorBig = rgba;
-        this.textBig = rgba;
+        this.colorBigRgba = rgba;
+        this.textBigRgba = rgba;
+        let rgb = that.getRgb(188, 8);
+        this.colorBigRgb = rgb;
+        this.textBigRgb = rgb;
         hideLoading();
       };
-      imgCanvas.addEventListener("click", that.pick);
+      imgCanvas.addEventListener('click', that.pick);
     },
     // 获取位置像素信息
     getRgba(x, y) {
       var pixel = this.ctxBig.getImageData(x, y, 1, 1);
       var data = pixel.data;
       // 获取rgba值
-      var rgba =
-        "rgba(" +
-        data[0] +
-        "," +
-        data[1] +
-        "," +
-        data[2] +
-        "," +
-        data[3] / 255 +
-        ")";
+      var rgba = 'rgba(' + data[0] + ',' + data[1] + ',' + data[2] + ',' + data[3] / 255 + ')';
       return rgba;
+    },
+    // 获取位置像素信息
+    getRgb(x, y) {
+      var pixel = this.ctxBig.getImageData(x, y, 1, 1);
+      var data = pixel.data;
+      // 获取rgb值
+      var rgb = 'rgb(' + data[0] + ',' + data[1] + ',' + data[2] + ')';
+      return rgb;
     },
     // 获取鼠标点击位置
     pick(event) {
       // 获取鼠标坐标
       var x = event.layerX;
       var y = event.layerY;
-      console.log("x", x, y);
+      console.log('x', x, y);
       // 获取图片像素信息
       let rgba = this.getRgba(x, y);
+      let rgb = this.getRgb(x, y);
       // 设置小正方形的背景颜色
-      this.colorBig = rgba;
-
+      this.colorBigRgba = rgba;
+      this.colorBigRgb = rgb;
       // 把拼接的字符串设置为元素的文本内容
-      this.textBig = rgba;
+      this.textBigRgba = rgba;
+      this.textBigRgb = rgb;
     },
     // rgb转HEX
     rgb2hex(rgb) {
@@ -87,9 +93,9 @@ export default {
       var arr = reg.exec(rgb);
 
       function hex(x) {
-        return ("0" + parseInt(x).toString(16)).slice(-2);
+        return ('0' + parseInt(x).toString(16)).slice(-2);
       }
-      var _hex = "#" + hex(arr[1]) + hex(arr[2]) + hex(arr[3]);
+      var _hex = '#' + hex(arr[1]) + hex(arr[2]) + hex(arr[3]);
       return _hex.toUpperCase();
     },
   },
@@ -99,12 +105,37 @@ export default {
   <el-dialog class="color-dialog" v-model="dialogVisible" title="取色器">
     <content class="bigImg-content">
       <div class="bigImg-left">
-        <img id="dialogImg" v-if="uploadFileBig" :src="uploadFileBig" alt=""/>
+        <img id="dialogImg" v-if="uploadFileBig" :src="uploadFileBig" alt="" />
         <canvas id="dialogImgCanvas"></canvas>
       </div>
       <div class="colorInfo-right">
-        <span class="color" :style="'background:' + colorBig"></span>
-        <span class="text">{{ textBig }}</span>
+        <el-row :gutter="20">
+          <el-col :span="8" style="text-align: center">
+            <span class="text">色块</span>
+            <div class="grid-content ep-bg-purple" />
+          </el-col>
+          <el-col :span="16">
+            <span class="text">色值</span>
+          </el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="8" style="text-align: center">
+            <span class="color" :style="'background:' + colorBigRgba"></span>
+            <div class="grid-content ep-bg-purple" />
+          </el-col>
+          <el-col :span="16">
+            <span class="text">{{ textBigRgba }}</span>
+          </el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="8" style="text-align: center">
+            <span class="color" :style="'background:' + colorBigRgb"></span>
+            <div class="grid-content ep-bg-purple" />
+          </el-col>
+          <el-col :span="16">
+            <span class="text">{{ textBigRgb }}</span>
+          </el-col>
+        </el-row>
       </div>
     </content>
   </el-dialog>
@@ -113,32 +144,36 @@ export default {
 #dialogImg {
   opacity: 0;
   width: 100%;
-  margin-bottom: 24px;
-  margin-left: 24px;
 }
 
 #dialogImgCanvas {
   position: absolute;
-  top: 0;
-  left: 24px;
-  z-index: 1
+  z-index: 1;
 }
-.bigImg-content{
+
+.bigImg-content {
   display: flex;
   justify-content: left;
-}
-.bigImg-left{
-  width: 70%
-}
-.colorInfo-right{
-  width: calc(30% - 24px)
-  padding 24px
+  height: 100%;
 }
 
+.bigImg-left {
+  width: 70%;
+  height: 100%;
+  padding: 24px;
+  display: flex;
+  align-items: center;
+}
 
 .colorInfo-right {
+  width: 30%;
+  height: 100%;
+  padding: 24px;
+  box-shadow: var(--box-shadow);
   display: flex;
-  justify-content: space-evenly;
+  justify-content: left;
+  flex-wrap: wrap;
+  align-content: flex-start;
   background: rgba(0, 0, 0, 0);
 
   .color {
@@ -146,13 +181,27 @@ export default {
     height: 40px;
     width: 40px;
     border-radius: 50%;
+    margin: 0 auto;
   }
 
   .text {
-    padding-left: 10px;
     line-height: 40px;
     font-size: 20px;
     font-weight: 600;
+  }
+
+  .el-row {
+    width: 100%;
+    margin: 0 !important;
+  }
+
+  .el-col {
+    text-align: center;
+    padding-bottom: 24px !important;
+    border-right: 1px dashed var(--vt-c-divider-light-2);
+    &:last-child{
+      border-right: 0px dashed var(--vt-c-divider-light-2);
+    }
   }
 }
 </style>
